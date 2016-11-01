096fae5a4ebe9daecb4ba07ae5857cf5ed186478
Vladimir Kovalev <kovalev.vladimir1990@gmail.com>
2016-11-01 21:09:33 +0000

    Add error messages

--- app/views.py
+++ app/views.py
@@ -12,13 +12,14 @@ def index():
 
 @app.route('/estimate', methods=['POST'])
 def estimate():
-    if any(key not in request.form for key in ['my_hand', 'opponent_count', 'table']):
-        return abort(400)
+    essential_fields = ['my_hand', 'opponent_count', 'table']
+    if any(key not in request.form for key in essential_fields):
+        return abort(400, "one of the following fields has not been specified - {}".format(','.join(essential_fields)))
     else:
         try:
             my_hand = card_list_from_comma_separated_string(request.values['my_hand'])
             table_cards = card_list_from_comma_separated_string(request.values['table'])
             opponent_count = int(request.form['opponent_count'])
             return jsonify(probability=probability_of_winning(my_hand, opponent_count, table_cards))
-        except ValueError:
-            abort(400)
+        except ValueError as e:
+            abort(400, str(e))
