47819a1bffe1e97d64c17b19c298f7ab58ecfb60
Vladimir Kovalev <kovalev.vladimir1990@gmail.com>
2016-12-17 23:31:39 +0000

    reverting parallelization. Seems like there are some issues with multiprocessing. Also this doesn't help much since heroku only gives me a single thread.

--- app/poker/simulator.py
+++ app/poker/simulator.py
@@ -4,13 +4,23 @@ from card import Card
 from deck import Deck
 from seven_card_hand import SevenCardHand
 from app.util import check_argument
-from multiprocessing import Pool, cpu_count
 
-_ITERATIONS = 2000 / cpu_count()
+_ITERATIONS = 1000
 
 
-def win_count(args):
-    my_hand, undealt_cards, opponent_count, table_cards = args
+def probability_of_winning(my_hand, opponent_count, table_cards):
+
+    check_argument(len(my_hand) == 2, "A player's hand must have 2 cards exactly")
+    check_argument(len(table_cards) in (0, 3, 4, 5), "A table can only have 0, 3, 4 or 5 cards at any given time")
+    check_argument(0 < opponent_count < 23, "The number of opponents must be between 1 and 23, inclusive")
+
+    duplicated_cards = [card for card, count in Counter(my_hand + table_cards).items() if count > 1]
+    check_argument(len(duplicated_cards) == 0,
+                   "The following cards appeared multiple times in table_cards + players_hand - {}".format(",".join(duplicated_cards)))
+
+    all_cards = {Card(suit, rank) for suit in "HDSC" for rank in "23456789TJQKA"}
+    undealt_cards = list(all_cards - set(my_hand + table_cards))
+
     my_wins = 0
     for i in xrange(_ITERATIONS):
         remaining_deck = Deck(undealt_cards)
@@ -28,22 +38,5 @@ def win_count(args):
 
         if my_best_five_card_hand.score() > best_opponent_five_card_subhand_of_all.score():
             my_wins += 1
-    return my_wins
 
-
-def probability_of_winning(my_hand, opponent_count, table_cards):
-
-    check_argument(len(my_hand) == 2, "A player's hand must have 2 cards exactly")
-    check_argument(len(table_cards) in (0, 3, 4, 5), "A table can only have 0, 3, 4 or 5 cards at any given time")
-    check_argument(0 < opponent_count < 23, "The number of opponents must be between 1 and 23, inclusive")
-
-    duplicated_cards = [str(card) for card, count in Counter(my_hand + table_cards).items() if count > 1]
-    check_argument(len(duplicated_cards) == 0,
-                   "The following cards appeared multiple times in table_cards + players_hand - {}".format(",".join(duplicated_cards)))
-
-    all_cards = {Card(suit, rank) for suit in "HDSC" for rank in "23456789TJQKA"}
-    undealt_cards = list(all_cards - set(my_hand + table_cards))
-
-    process_pool = Pool(cpu_count())
-    total_wins = sum(process_pool.map(win_count, [[my_hand, undealt_cards, opponent_count, table_cards]] * cpu_count()))
-    return float(total_wins) / float(_ITERATIONS * cpu_count())
+    return float(my_wins) / float(_ITERATIONS)
