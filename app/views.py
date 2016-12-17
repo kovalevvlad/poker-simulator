0453335b0e281a4743bc180856d24fabbc2f85e3
Vladimir Kovalev <kovalev.vladimir1990@gmail.com>
2016-12-17 23:34:14 +0000

    moving the endpoint to GET. What was I thinking?

--- app/views.py
+++ app/views.py
@@ -10,16 +10,16 @@ def index():
     return "Hello, Poker World!"
 
 
-@app.route('/estimate', methods=['POST'])
+@app.route('/estimate', methods=['GET'])
 def estimate():
     essential_fields = ['my_hand', 'opponent_count', 'table']
-    if any(key not in request.form for key in essential_fields):
+    if any(key not in request.args for key in essential_fields):
         return abort(400, "one of the following fields has not been specified - {}".format(','.join(essential_fields)))
     else:
         try:
-            my_hand = card_list_from_comma_separated_string(request.values['my_hand'])
-            table_cards = card_list_from_comma_separated_string(request.values['table'])
-            opponent_count = int(request.form['opponent_count'])
+            my_hand = card_list_from_comma_separated_string(request.args.get('my_hand'))
+            table_cards = card_list_from_comma_separated_string(request.args.get('table'))
+            opponent_count = int(request.args.get('opponent_count'))
             return jsonify(probability=probability_of_winning(my_hand, opponent_count, table_cards))
         except ValueError as e:
             abort(400, str(e))
